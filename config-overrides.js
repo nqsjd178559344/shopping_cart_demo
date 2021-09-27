049db41efb5455b28f5c36539355b9bffb03b6c1
nqsjd178559344 <1401444513@qq.com>
2021-09-27 17:16:20 +0800

    feat: webpack-bundle-analyze

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,13 @@
 /* config-overrides.js */
+const BundleAnalyzer = require("webpack-bundle-analyzer").BundleAnalyzerPlugin // 输出dist每个文件的大小的树形图
 module.exports = function override(config, env) {
     //do stuff with the webpack config...
     return config;
 }
+
+// module.exports = {
+//     //do stuff with the webpack config...
+//     plugins: [
+//         new BundleAnalyzer()
+//     ]
+// }
