fe734a5c3ae66e7ba5502fcd11fa03a2247435b7
nqsjd178559344 <1401444513@qq.com>
2021-10-11 19:05:20 +0800

    feat:customize-cra

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,14 @@
 /* config-overrides.js */
-const BundleAnalyzer = require("webpack-bundle-analyzer").BundleAnalyzerPlugin // 输出dist每个文件的大小的树形图
+// const BundleAnalyzer = require("webpack-bundle-analyzer").BundleAnalyzerPlugin // 输出dist每个文件的大小的树形图
 // 方法1
 const rewireLess = require('@joiner/react-app-rewire-less-modules');
 // 方法2
 // const rewireLess = require('@joiner/react-app-rewire-less');// todo 找不到包
 // ? 1/2使用方法相同 => 均需要加入less-loader@^4.1.0
 
-module.exports = function override(config, env) {
+const { override, addDecoratorsLegacy, addLessLoader } = require('customize-cra');
+
+function configFun(config, env) {
     //do stuff with the webpack config...
     config = rewireLess(config, env);
     // let loaders = config.module.rules.find(rule => Array.isArray(rule.oneOf)).oneOf
@@ -43,7 +45,17 @@ module.exports = function override(config, env) {
     //     ]
     // })
     if (config.plugins) {
-        config.plugins.push(new BundleAnalyzer())
+        // config.plugins.push(new BundleAnalyzer())
     }
     return config;
 }
+
+module.exports = override(
+    addDecoratorsLegacy(),
+    addLessLoader()
+    // addLessLoader({
+    //     lessOptions: {
+    //         javascriptEnabled: true
+    //     }
+    // }),
+)
--- src/store/cart.js
+++ src/store/cart.js
@@ -1,41 +1,44 @@
-import { observable, action, decorate } from 'mobx'
+import { observable, action } from 'mobx'
 
 class CartStore {
 
-    list = [];
+    @observable
+    list = []
 
+    @action
     handleAdd = (item) => {
         let num = 1
-        if(item.num > 1){
+        if (item.num > 1) {
             num = item.num + 1
         }
 
-        const hasItemBefore = this.list.find(i=>i.id === item.id)
+        const hasItemBefore = this.list.find(i => i.id === item.id)
         // 新加入的项在最上面
-        if(hasItemBefore){ // 之前存在
-            this.list = this.list.map(i=>{
-                if(i.id === item.id){
-                    i.num ++
+        if (hasItemBefore) { // 之前存在
+            this.list = this.list.map(i => {
+                if (i.id === item.id) {
+                    i.num++
                 }
                 return i
             })
-        }else{
-            const target = {...item,num}
+        } else {
+            const target = { ...item, num }
             this.list.push(target);
         }
     }
 
     // action
+    @action
     handleDel(item) {
         this.list = this.list.filter(i => i.id !== item.id);
     }
 }
 
-decorate(CartStore, {
-    list: observable,
-    handleAdd: action,
-    handleDel: action,
-});
+// decorate(CartStore, {
+//     list: observable,
+//     handleAdd: action,
+//     handleDel: action,
+// });
 
 const cartStore = new CartStore()
 export default cartStore;
